d6473e2bc75424249e04d8a57de7ea86de061521
donut <sunruyun2@gmail.com>
2022-11-26 13:00:03 +0000

    figure out onPause and onResume

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -5,26 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //ClockSurfaceView clock;
-    TimerSurfaceView clock;
+    ClockSurfaceView clock;
+    TimerSurfaceView timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //get the size of the screen
 
-        //clock = new ClockSurfaceView(this , 300);
-        clock = new TimerSurfaceView(this, 300 , 60);
-        setContentView(clock);
+        clock = new ClockSurfaceView(this , 300);
+        timer = new TimerSurfaceView(this, 300 , 60);
+        setContentView(timer);
     }
 
     @Override
     protected void onPause(){
         super.onPause();
         clock.onPauseClock();
+        timer.onPauseClock();
     }
     @Override
     protected void onResume(){
         super.onResume();
         clock.onResumeClock();
+        timer.onResumeClock();
     }
 }

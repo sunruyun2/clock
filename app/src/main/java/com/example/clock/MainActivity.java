9b90e7f07ee2811350f3f5c0ba75c5ac5eccc2e1
donut <sunruyun2@gmail.com>
2022-11-29 19:31:58 +0000

    add menu then next step is try to make it work

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -1,12 +1,38 @@
 package com.example.clock;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ClockSurfaceView clock;
     TimerSurfaceView timer;
+
+    //edit menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+
+        return true;
+    }
+    //edit menu activity
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "clock", Toast.LENGTH_SHORT).show();
+            case R.id.item2:
+                Toast.makeText(this, "timer", Toast.LENGTH_SHORT).show();
+            default:return super.onOptionsItemSelected(item);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

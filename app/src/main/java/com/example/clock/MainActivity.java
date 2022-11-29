21286babf124f0736e78f6f9552bc29849686578
donut <sunruyun2@gmail.com>
2022-11-29 19:45:32 +0000

    failed somehow

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,9 +29,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.item1:
-                Toast.makeText(this, "clock", Toast.LENGTH_SHORT).show();
+                try{
+                    clock.setVisibility(View.VISIBLE);
+                    timer.setVisibility(View.INVISIBLE);
+                } catch (Exception e){
+
+                }
+
             case R.id.item2:
-                Toast.makeText(this, "timer", Toast.LENGTH_SHORT).show();
+                try {
+                    clock.setVisibility(View.INVISIBLE);
+                    timer.setVisibility(View.VISIBLE);
+                } catch (Exception e){
+
+                }
             default:return super.onOptionsItemSelected(item);
         }
     }
@@ -44,8 +56,8 @@ public class MainActivity extends AppCompatActivity {
         clock = new ClockSurfaceView(this , 300);
         timer = new TimerSurfaceView(this, 300 , 60);
         mainLayout = (ConstraintLayout) findViewById(R.id.main_layout);
-        mainLayout.addView(clock);
         mainLayout.addView(timer);
+        mainLayout.addView(clock);
 
     }
 
